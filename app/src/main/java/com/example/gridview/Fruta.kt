a1ebd3c90b573bd56119469ca0287f8449d8b7b5
Josejimenez17 <joseluisssj14@gmail.com>
2022-03-07 09:51:11 -0500

    comenzando app

--- app/src/main/java/com/example/gridview/Fruta.kt
+++ app/src/main/java/com/example/gridview/Fruta.kt
@@ -3,7 +3,7 @@ package com.example.gridview
 class Fruta(nombre:String, imagen:Int) {
     var nombre: String=""
     var imagen:Int = 0
-
+//prueba
     init {
         this.nombre=nombre
         this.imagen=imagen
